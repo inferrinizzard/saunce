508b0bce146122739be741edc2863eb7bc99d0de
inferrinizzard <inferrinizzard@gmail.com>
2020-12-25 14:57:30 -0800

    type colorshift

--- src/util/ColorShift.ts
+++ src/util/ColorShift.ts
@@ -5,92 +5,73 @@
  */
 
 class Color {
-	constructor(r, g, b) {
+	r!: number;
+	g!: number;
+	b!: number;
+	constructor(r: number, g: number, b: number) {
 		this.set(r, g, b);
 	}
 
 	triple = () => [this.r, this.g, this.b];
-
+	set = (r: number, g: number, b: number) => ([this.r, this.g, this.b] = [r, g, b].map(x => this.clamp(x))); // prettier-ignore
 	toString = () => `rgb(${Math.round(this.r)}, ${Math.round(this.g)}, ${Math.round(this.b)})`;
-
-	set = (r, g, b) => ([this.r, this.g, this.b] = [r, g, b].map(x => this.clamp(x)));
 
 	hueRotate(angle = 0) {
 		angle = (angle / 180) * Math.PI;
 		const sin = Math.sin(angle);
 		const cos = Math.cos(angle);
 
-		this.multiply([
-			0.213 + cos * 0.787 - sin * 0.213,
-			0.715 - cos * 0.715 - sin * 0.715,
-			0.072 - cos * 0.072 + sin * 0.928,
-			0.213 - cos * 0.213 + sin * 0.143,
-			0.715 + cos * 0.285 + sin * 0.14,
-			0.072 - cos * 0.072 - sin * 0.283,
-			0.213 - cos * 0.213 - sin * 0.787,
-			0.715 - cos * 0.715 + sin * 0.715,
-			0.072 + cos * 0.928 + sin * 0.072,
-		]);
+		this.multiply(
+			((a, b, c) => a.map((x, i) => x + cos * b[i] + sin * c[i]))(
+				[0.213, 0.715, 0.072, 0.213, 0.715, 0.072, 0.213, 0.715, 0.072],
+				[+0.787, -0.715, -0.072, -0.213, +0.285, -0.072, -0.213, -0.715, +0.928],
+				[-0.213, -0.715, +0.928, +0.143, +0.14, -0.283, -0.787, +0.715, +0.072]
+			)
+		);
 	}
 
-	grayscale(value = 1) {
-		this.multiply([
-			0.2126 + 0.7874 * (1 - value),
-			0.7152 - 0.7152 * (1 - value),
-			0.0722 - 0.0722 * (1 - value),
-			0.2126 - 0.2126 * (1 - value),
-			0.7152 + 0.2848 * (1 - value),
-			0.0722 - 0.0722 * (1 - value),
-			0.2126 - 0.2126 * (1 - value),
-			0.7152 - 0.7152 * (1 - value),
-			0.0722 + 0.9278 * (1 - value),
-		]);
-	}
+	grayscale = (value = 1) =>
+		this.multiply(
+			((a, b) => a.map((x, i) => x + b[i] * (1 - value)))(
+				[0.2126, 0.7152, 0.0722, 0.2126, 0.7152, 0.0722, 0.2126, 0.7152, 0.0722],
+				[+0.7874, -0.7152, -0.0722, -0.2126, +0.2848, -0.0722, -0.2126, -0.7152, +0.9278]
+			)
+		);
 
-	sepia(value = 1) {
-		this.multiply([
-			0.393 + 0.607 * (1 - value),
-			0.769 - 0.769 * (1 - value),
-			0.189 - 0.189 * (1 - value),
-			0.349 - 0.349 * (1 - value),
-			0.686 + 0.314 * (1 - value),
-			0.168 - 0.168 * (1 - value),
-			0.272 - 0.272 * (1 - value),
-			0.534 - 0.534 * (1 - value),
-			0.131 + 0.869 * (1 - value),
-		]);
-	}
+	sepia = (value = 1) =>
+		this.multiply(
+			((a, b) => a.map((x, i) => x + b[i] * (1 - value)))(
+				[0.393, 0.769, 0.189, 0.349, 0.686, 0.168, 0.272, 0.534, 0.131],
+				[+0.607, -0.769, -0.189, -0.349, +0.314, -0.168, -0.272, -0.534, +0.869]
+			)
+		);
 
-	saturate(value = 1) {
-		this.multiply([
-			0.213 + 0.787 * value,
-			0.715 - 0.715 * value,
-			0.072 - 0.072 * value,
-			0.213 - 0.213 * value,
-			0.715 + 0.285 * value,
-			0.072 - 0.072 * value,
-			0.213 - 0.213 * value,
-			0.715 - 0.715 * value,
-			0.072 + 0.928 * value,
-		]);
-	}
+	saturate = (value = 1) =>
+		this.multiply(
+			((a, b) => a.map((x, i) => x + b[i] * value))(
+				[0.213, 0.715, 0.072, 0.213, 0.715, 0.072, 0.213, 0.715, 0.072],
+				[+0.787, -0.715, -0.072, -0.213, +0.285, -0.072, -0.213, -0.715, +0.928]
+			)
+		);
 
-	multiply = matrix =>
-		this.set(...[0, 3, 6].map(j => this.triple().reduce((a, x, i) => a + x * matrix[i + j], 0)));
+	multiply = (matrix:number[]) =>
+		this.set(...[0, 3, 6].map(j => this.triple().reduce((a, x, i) => a + x * matrix[i + j], 0)) as [number, number, number]); //prettier-ignore
 
 	brightness = (value = 1) => this.linear(value);
+
 	contrast = (value = 1) => this.linear(value, -(0.5 * value) + 0.5);
 
 	linear = (slope = 1, intercept = 0) =>
-		this.set(...this.triple().map(x => x * slope + intercept * 255));
+		this.set(...(this.triple().map(x => x * slope + intercept * 255) as [number, number, number]));
 
 	invert = (value = 1) =>
-		this.set(...this.triple().map(x => (value + (x / 255) * (1 - 2 * value)) * 255));
+		this.set(...this.triple().map(x => (value + (x / 255) * (1 - 2 * value)) * 255) as [number, number, number]); //prettier-ignore
 
-	hsl() {
+	hsl(): HSL {
 		// Code taken from https://stackoverflow.com/a/9493060/2688027, licensed under CC BY-SA.
 		const [r, g, b] = this.triple().map(x => x / 255);
-		const max = Math.max(r, g, b);
+		const maxPair = Object.entries({ r, g, b }).reduce((a, [k, v]) => (a.v > v ? a : { k, v }), { k: '', v: 0 }); // prettier-ignore
+		const max = maxPair.v;
 		const min = Math.min(r, g, b);
 		let h, s, l;
 		l = (max + min) / 2;
@@ -100,21 +81,11 @@ class Color {
 		} else {
 			const d = max - min;
 			s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-			switch (max) {
-				case r:
-					h = (g - b) / d + (g < b ? 6 : 0);
-					break;
-
-				case g:
-					h = (b - r) / d + 2;
-					break;
-
-				case b:
-					h = (r - g) / d + 4;
-					break;
-				default:
-					break;
-			}
+			h = ({
+				r: (g - b) / d + (g < b ? 6 : 0),
+				g: (b - r) / d + 2,
+				b: (r - g) / d + 4,
+			} as { [x: string]: number })[maxPair.k];
 			h /= 6;
 		}
 
@@ -122,11 +93,17 @@ class Color {
 		return { h, s, l };
 	}
 
-	clamp = value => (value > 255 && 255) || (value < 0 && 0) || value;
+	clamp = (value: number) => (value > 255 && 255) || (value < 0 && 0) || value;
 }
 
+type HSL = { h: number; s: number; l: number };
+type SPSALoss = { values: number[]; loss: number };
+
 class Solver {
-	constructor(target, baseColor) {
+	target: Color;
+	targetHSL: HSL;
+	reusedColor: Color;
+	constructor(target: Color) {
 		this.target = target;
 		this.targetHSL = target.hsl();
 		this.reusedColor = new Color(0, 0, 0);
@@ -142,34 +119,37 @@ class Solver {
 	}
 
 	solveWide() {
-		const A = 5;
-		const c = 15;
+		const [A, c] = [5, 15];
 		const a = [60, 180, 18000, 600, 1.2, 1.2];
 
-		let best = { loss: Infinity };
-		for (let i = 0; best.loss > 25 && i < 3; i++) {
-			const initial = [50, 20, 3750, 50, 100, 100];
-			const result = this.spsa(A, a, c, initial, 1000);
-			if (result.loss < best.loss) best = result;
-		}
+		const initial = [50, 20, 3750, 50, 100, 100];
+		let best: SPSALoss = { values: initial, loss: Infinity };
+		for (let i = 0; best.loss > 25 && i < 3; i++)
+			best = [best, this.spsa(A, a, c, initial, 1000)].reduce((acc, res) =>
+				acc.loss < res.loss ? acc : res
+			);
 		return best;
 	}
 
-	solveNarrow(wide) {
-		const A = wide.loss;
-		const c = 2;
+	solveNarrow(wide: SPSALoss) {
+		const [A, c] = [wide.loss, 2];
 		const A1 = A + 1;
 		const a = [0.25 * A1, 0.25 * A1, A1, 0.25 * A1, 0.2 * A1, 0.2 * A1];
 		return this.spsa(A, a, c, wide.values, 500);
 	}
 
-	spsa(A, a, c, values, iters) {
-		const alpha = 1;
-		const gamma = 0.16666666666666666;
+	spsa(A: number, a: number[], c: number, values: number[], iters: number): SPSALoss {
+		const [alpha, gamma] = [1, 1.0 / 6];
 
-		let best = null;
-		let bestLoss = Infinity;
+		let best: SPSALoss = { values: [], loss: Infinity };
 		const [deltas, highArgs, lowArgs] = [0, 0, 0].map(_ => new Array(6));
+
+		let fix = (value: number, i: number) => {
+			let max = ({ 2: 7500, 4: 200, 5: 200 } as { [x: number]: number })[i] || 100;
+			return (
+				(value > max && max) || (value < 0 && (value = i === 3 ? max + (value % max) : 0)) || value
+			);
+		};
 
 		for (let k = 0; k < iters; k++) {
 			const ck = c / Math.pow(k + 1, gamma);
@@ -187,24 +167,12 @@ class Solver {
 			}
 
 			const loss = this.loss(values);
-			if (loss < bestLoss) {
-				best = values.slice(0);
-				bestLoss = loss;
-			}
+			if (loss < best.loss) best = { values, loss };
 		}
-		return { values: best, loss: bestLoss };
-
-		function fix(value, idx) {
-			let max = { 2: 7500, 4: 200, 5: 200 }[idx] || 100;
-			return (
-				(value > max && max) ||
-				(value < 0 && (value = idx === 3 ? max + (value % max) : 0)) ||
-				value
-			);
-		}
+		return best;
 	}
 
-	loss(filters) {
+	loss(filters: number[]) {
 		// Argument is array of percentages.
 		const color = this.reusedColor;
 		color.set(0, 0, 0);
@@ -227,7 +195,7 @@ class Solver {
 		);
 	}
 
-	css = filters =>
+	css = (filters: number[]) =>
 		['invert', 'sepia', 'saturate', 'hue-rotate', 'brightness', 'contrast'].reduce(
 			(acc, f, i) =>
 				` ${acc} ${f}(${Math.round(filters[i] * (i === 3 ? 3.6 : 1))}${i === 3 ? 'deg' : '%'})`,
@@ -235,7 +203,7 @@ class Solver {
 		) + ';';
 }
 
-const hexToRgb = hex => {
+const hexToRgb = (hex: string) => {
 	// Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
 	const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
 	hex = hex.replace(shorthandRegex, (m, ...colour) => colour.reduce((a, x) => a + x + x, ''));
@@ -244,9 +212,9 @@ const hexToRgb = hex => {
 	return result ? result.slice(1).map(x => parseInt(x, 16)) : null;
 };
 
-const ShiftColour = colour => {
-	let solver = new Solver(new Color(...hexToRgb(colour)));
-	let result = { loss: Infinity };
+const ShiftColour = (colour: string) => {
+	let solver = new Solver(new Color(...(hexToRgb(colour) as [number, number, number])));
+	let result = { loss: Infinity, filter: '' };
 	while (result.loss > 15) result = solver.solve();
 	return result.filter;
 };
