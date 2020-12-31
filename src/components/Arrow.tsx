a92c0107197d45c6577a33466cb141eb3c9762f3
inferrinizzard <inferrinizzard@gmail.com>
2020-12-31 03:52:34 -0800

    basic manual mappings

--- src/components/Arrow.tsx
+++ src/components/Arrow.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
 
-import { Pos } from './Card';
+import { Pos, CardBlockSize } from './Card';
 
 export interface ArrowProps {
 	head?: Pos;
 	tail?: Pos;
 	colour: string;
+	heightOverride?: number;
 }
 
-const Arrow: React.FC<ArrowProps> = ({ head, tail, colour }) => {
+const Arrow: React.FC<ArrowProps> = ({ head, tail, colour, heightOverride }) => {
 	if (!head || !tail) return null;
 	const dx = head.x - tail.x;
-	const dy = head.y - tail.y;
+	let dy = head.y - tail.y;
+	let y = 0;
+	if (head.y - tail.y > CardBlockSize.y || heightOverride) {
+		dy = heightOverride
+			? head.y - tail.y - CardBlockSize.y * heightOverride
+			: (head.y - tail.y) % CardBlockSize.y;
+		y = (heightOverride ?? Math.floor((head.y - tail.y) / CardBlockSize.y)) * CardBlockSize.y;
+	}
 	const controlDist = dy;
 	const width = 20;
 	return (
 		<svg
 			width={Math.abs(dx) + width * 2}
-			height={Math.abs(dy)}
+			height={Math.abs(dy + y)}
 			style={{
 				zIndex: -1,
 				position: 'fixed',
@@ -30,28 +38,34 @@ const Arrow: React.FC<ArrowProps> = ({ head, tail, colour }) => {
 					(dx &&
 						(dx > 0
 							? `M ${width / 2} 0
+							v ${y}
 							c 0 ${controlDist} ${dx} ${dy - controlDist} ${dx} ${dy - width}
 							h ${-width / 2}
 							l ${width} ${width}
 							l ${width} ${-width}
 							h ${-width / 2}
-							c 0 ${-controlDist} ${-dx} ${-dy + controlDist} ${-dx + width / 2} ${-dy + width}
+							c 0 ${-controlDist} ${-dx} ${-dy + controlDist} ${-dx} ${-dy + width}
+							v ${-y}
 							z`
 							: `M ${width / 2 - dx} 0
+							v ${y}
 							c 0 ${controlDist - width} ${dx} ${dy - controlDist - width} ${dx} ${dy - width}
 							h ${-width / 2}
 							l ${width} ${width}
 							l ${width} ${-width}
 							h ${-width / 2}
-							c 0 ${-controlDist + width} ${-dx} ${-dy + controlDist + width} ${-dx + width / 2} ${-dy + width}
+							c 0 ${-controlDist + width} ${-dx} ${-dy + controlDist + width} ${-dx} ${-dy + width}
+							v ${-y}
 							z`)) ||
 					`M ${width / 2} 0 
+							v ${y}
 							v ${dy - width} 
 							h ${-width / 2}
 							l ${width} ${width}
 							l ${width} ${-width}
 							h ${-width / 2}
 							v ${-dy + width}
+							v ${-y}
 							z`
 				}
 				fill={colour}
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -44,13 +44,23 @@ const Main: React.FC<MainProps> = () => {
 		sauce: keyof typeof filles,
 		num: number,
 		posFunction: (i: number) => Pos,
-		flags?: { filter: boolean }
+		flags?: { filter?: boolean; sliceStart?: number }
 	) =>
 		(filles[sauce] as SauceName[])
 			.filter(fille => !flags?.filter || !connected.includes(fille))
-			.splice(0, num)
+			.splice(flags?.sliceStart ?? 0, num)
 			.map((fille, i) => (
-				<Card key={fille} name={fille} pos={posFunction(i)} attach={attachAnchor} />
+				<React.Fragment key={fille}>
+					<Card key={fille} name={fille} pos={posFunction(i)} attach={attachAnchor} />
+					{filles[fille as keyof typeof filles]?.length === 1 && (
+						<Card
+							key={fille}
+							name={filles[fille as keyof typeof filles][0]}
+							pos={{ x: posFunction(i).x, y: posFunction(i).y + 1 }}
+							attach={attachAnchor}
+						/>
+					)}
+				</React.Fragment>
 			));
 
 	return (
@@ -68,51 +78,86 @@ const Main: React.FC<MainProps> = () => {
 			/>
 
 			<Card name="espagnole" pos={{ x: 0, y: 0 }} attach={attachAnchor} />
-			{Tree('espagnole', 8, i => ({ x: -(3 - (i % 3)), y: 1 + Math.floor(i / 3) }), {
+			{Tree('espagnole', 9, i => ({ x: -(3 - (i % 3)), y: 1 + Math.floor(i / 3) }), {
 				filter: false,
 			})}
-			{/* {(filles['espagnole'] as SauceName[])
-				.filter(fille => !connected.includes(fille))
-				.splice(0, 9)
-				.map((fille, i) => (
-					<Card
-						key={fille}
-						name={fille}
-						pos={{ x: -(3 - (i % 3)), y: 1 + Math.floor(i / 3) }}
-						attach={attachAnchor}
-					/>
-				))} */}
-			<Card name="romaine" pos={{ x: -3, y: 4 }} attach={attachAnchor} />
-			<Card name="vin rouge" pos={{ x: -2, y: 4 }} attach={attachAnchor} />
-			<Card name="bercy" pos={{ x: 1, y: 3 }} attach={attachAnchor} />
-			<Card name="marinière" pos={{ x: 1, y: 4 }} attach={attachAnchor} />
-			<Card name="demi-glace" pos={{ x: 0, y: 4 }} attach={attachAnchor} />
-			<Card name="gratin" pos={{ x: -1, y: 5 }} attach={attachAnchor} />
+			{/* <Arrow
+				colour={'salmon'}
+				tail={anchors['espagnole']?.out}
+				head={anchors['gratin']?.in}
+				heightOverride={2}
+			/> */}
+			<Card name="vin rouge" pos={{ x: 1, y: 3 }} attach={attachAnchor} />
+			<Card name="bercy" pos={{ x: 1, y: 4 }} attach={attachAnchor} />
+			<Card name="marinière" pos={{ x: 1, y: 5 }} attach={attachAnchor} />
+
+			<Card name="demi-glace" pos={{ x: -1, y: 4 }} attach={attachAnchor} />
+			<Card name="gratin" pos={{ x: 0, y: 5 }} attach={attachAnchor} />
+			{Tree('demi-glace', 21, i => ({ x: -4 + (i % 6), y: 6 + Math.floor(i / 6) }), {
+				sliceStart: 1,
+			})}
+			{/* <Card name="poivrade" pos={{ x: 0, y: 9 }} attach={attachAnchor} />
+			{Tree('poivrade', 4, i => ({ x: -1 + i, y: 10 }))} */}
 
 			<Card name="tomate" pos={{ x: -4, y: 0 }} attach={attachAnchor} />
 			{Tree('tomate', 3, i => ({ x: -5, y: 1 + i }))}
 			<Card name="zingara" pos={{ x: -4, y: 5 }} attach={attachAnchor} />
 
 			<Card name="velouté de poisson" pos={{ x: 2, y: 0 }} attach={attachAnchor} />
-			<Card name="crevettes" pos={{ x: 4, y: 1 }} attach={attachAnchor} />
-			<Card name="bonnefoy" pos={{ x: 1, y: 1 }} attach={attachAnchor} />
-			<Card name="bretonne" pos={{ x: 3, y: 1 }} attach={attachAnchor} />
-			<Card name="l'aurore" pos={{ x: 1, y: 2 }} attach={attachAnchor} />
-			<Card name="livonienne" pos={{ x: 3, y: 2 }} attach={attachAnchor} />
-			<Card name="nantua" pos={{ x: 4, y: 2 }} attach={attachAnchor} />
-			<Card name="vénitienne" pos={{ x: 3, y: 3 }} attach={attachAnchor} />
-			<Card name="vin blanc" pos={{ x: 4, y: 3 }} attach={attachAnchor} />
+			{Tree(
+				'velouté de poisson',
+				8,
+				i => ({
+					x: 2 + ((8 - i) % 3 || -1),
+					y: 1 + Math.floor(i / 3),
+				}),
+				{ filter: false }
+			)}
+
+			<Card name="américaine" pos={{ x: 3, y: 4 }} attach={attachAnchor} />
+			<Card name="homard" pos={{ x: 4, y: 4 }} attach={attachAnchor} />
+			<Card name="orientale" pos={{ x: 3, y: 5 }} attach={attachAnchor} />
+			<Card name="victoria" pos={{ x: 4, y: 5 }} attach={attachAnchor} />
+
 			<Card name="normande" pos={{ x: 2, y: 5 }} attach={attachAnchor} />
-			<Card name="homard" pos={{ x: 3, y: 4 }} attach={attachAnchor} />
-			<Card name="américaine" pos={{ x: 4, y: 4 }} attach={attachAnchor} />
+			{Tree('normande', 4, i => ({ x: 2 + (i % 2), y: 6 + Math.floor(i / 2) }))}
 
 			<Card name="béchamel" pos={{ x: 5, y: 0 }} attach={attachAnchor} />
-			<Card name="bohémienne" pos={{ x: 6, y: 1 }} attach={attachAnchor} />
-			<Card name="cardinal" pos={{ x: 6, y: 2 }} attach={attachAnchor} />
-			<Card name="mornay" pos={{ x: 6, y: 3 }} attach={attachAnchor} />
+			{Tree('béchamel', 4, i => ({ x: 6, y: 1 + i }))}
 			{/* <Card name="moutarde" pos={{ x: 7, y: 2 }} attach={attachAnchor} /> */}
-			<Card name="soubise" pos={{ x: 6, y: 4 }} attach={attachAnchor} />
 			<Card name="crème" pos={{ x: 5, y: 5 }} attach={attachAnchor} />
+
+			<Card name="hollandaise" pos={{ x: 7, y: 0 }} attach={attachAnchor} />
+			{Tree('hollandaise', 4, i => ({ x: 8 + (i % 2), y: 1 + Math.floor(i / 2) }))}
+			<Card name="béarnaise" pos={{ x: 7, y: 2 }} attach={attachAnchor} />
+			{Tree('béarnaise', 4, i => ({ x: 8 + (i % 2), y: 3 + Math.floor(i / 2) }))}
+			<Card name="tyrolienne" pos={{ x: 7, y: 4 }} attach={attachAnchor} />
+
+			<Card name="velouté de veau" pos={{ x: 10, y: 0 }} attach={attachAnchor} />
+			{Tree('velouté de veau', 4, i => ({ x: 10 + 1 + (i % 2), y: 1 + Math.ceil(i / 2) }))}
+			<Card name="allemande" pos={{ x: 11, y: 3 }} attach={attachAnchor} />
+			<Card name="poulette" pos={{ x: 10, y: 4 }} attach={attachAnchor} />
+
+			<Card name="velouté de volaille" pos={{ x: 13, y: 0 }} attach={attachAnchor} />
+			{Tree('velouté de volaille', 4, i => ({
+				x: 13 + (i ? i % 2 : -1),
+				y: 1 + Math.floor(i / 2),
+			}))}
+
+			<Card name="mayonnaise" pos={{ x: 15, y: 0 }} attach={attachAnchor} />
+			{Tree('mayonnaise', 6, i => ({ x: 16 + (i % 3), y: 1 + Math.floor(i / 3) }))}
+			{Tree('mayonnaise', 5, i => ({ x: 14 + i, y: 3 }), { sliceStart: 6 })}
+
+			{Object.entries(filles).map(([mère, filles]) =>
+				filles.map(fille => (
+					<Arrow
+						key={fille}
+						colour={'salmon'}
+						tail={anchors[mère]?.out}
+						head={anchors[fille]?.in}
+					/>
+				))
+			)}
 		</div>
 	);
 };
