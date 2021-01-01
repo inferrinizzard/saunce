224d8da82a5cd040aadbf67235b846dcc75a40f0
inferrinizzard <inferrinizzard@gmail.com>
2021-01-01 02:43:50 -0800

    add remaining sauces

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import Positions from '../data/pos.json';
 import filles from '../data/filles.json';
+import sauces from '../data/sauce.json';
 import { dupes } from '../data/notes.json';
 const connected = Object.values(filles)
 	.reduce((all, vals) => [...all, ...vals.filter(v => Object.keys(filles).includes(v))], [])
@@ -16,6 +17,9 @@ const Main: React.FC<MainProps> = () => {
 	let [cards, setCards] = useState({} as { [k: string]: Card });
 	let linkCard = (sauce: Card, force: boolean = false) =>
 		(!cards[sauce.name] || force) && setCards(prev => ({ ...prev, [sauce.name]: sauce }));
+
+	let remaining = Object.keys(sauces).filter(sauce => !Object.keys(Positions).includes(sauce));
+	console.log(remaining.filter(sauce => Object.keys(filles).includes(sauce)));
 
 	return (
 		<div
@@ -69,6 +73,42 @@ const Main: React.FC<MainProps> = () => {
 			{Object.entries(Positions).map(([sauce, pos]) => (
 				<Card key={sauce} name={sauce as SauceName} pos={pos} attach={linkCard} />
 			))}
+
+			{remaining
+				.filter(
+					sauce =>
+						!Object.keys(filles).includes(sauce) &&
+						!Object.values(filles)
+							.reduce((arr, cur) => [...arr, ...cur], [])
+							.includes(sauce)
+				)
+				.map((sauce, i) => (
+					<Card
+						key={sauce}
+						name={sauce as SauceName}
+						pos={{ x: -5 + i, y: 12 }}
+						attach={linkCard}
+					/>
+				))}
+			{['poivrade', 'beurre', 'vinaigrette'].map((mère, j) => (
+				<React.Fragment key={mère}>
+					<Card name={mère as SauceName} pos={{ x: -5, y: 4 + 2 * j }} attach={linkCard} />
+					{(filles[mère as keyof typeof filles] as SauceName[]).map((fille, i) => (
+						<Card
+							key={fille}
+							name={fille}
+							pos={{ x: -5 - i, y: 4 + 2 * j + 1 }}
+							attach={linkCard}
+						/>
+					))}
+				</React.Fragment>
+			))}
+			<Card
+				key={'glace de viande'}
+				name={'glace de viande'}
+				pos={{ x: -1, y: 9 }}
+				attach={linkCard}
+			/>
 
 			{Object.entries(filles).map(([mère, filles]) =>
 				filles.map(fille => (
