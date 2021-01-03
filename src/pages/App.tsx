79265af0576aecdecff8de93efd2fe9b300edc09
inferrinizzard <inferrinizzard@gmail.com>
2021-01-03 07:56:11 -0800

    fix crème icon, sort cards, update some desc

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -8,6 +8,7 @@ import '../css/ingredients.css';
 
 import Overlay from '../components/Overlay';
 import Main from '../components/Main';
+import { CardBlockSize } from '../components/Card';
 
 let theme = {
 	offwhite: '#f7f7f2', // #faf3dd
@@ -30,8 +31,18 @@ export default function App() {
 				<TransformWrapper
 					defaultPositionX={0}
 					defaultPositionY={0}
-					wheel={{ step: 10 }}
-					options={{ minScale: 0.5, centerContent: false, limitToBounds: false }}>
+					wheel={{ step: 15 }}
+					// onPanningStop={(e: any) => console.log(e)}
+					options={{
+						minScale: 0.35,
+						maxScale: 1,
+						centerContent: false,
+						limitToBounds: false,
+						maxPositionX: 11 * CardBlockSize.x,
+						minPositionX: -12 * CardBlockSize.x,
+						maxPositionY: 1 * CardBlockSize.y,
+						minPositionY: -10 * CardBlockSize.y,
+					}}>
 					<TransformComponent>
 						<Main />
 					</TransformComponent>
